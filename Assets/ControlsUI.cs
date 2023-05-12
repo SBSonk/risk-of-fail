6f63b12d7939b31a913d74bb3a92d997de448c8d
SBSonk <sg.buenaventura04@gmail.com>
2023-05-12 21:16:49 +0800

    Key Rebinding Setup

--- Assets/ControlsUI.cs
+++ Assets/ControlsUI.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ControlsUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject setBindUI;
+    public TextMeshProUGUI bindTimer;
+
+
+    private string keyChanging;
+    private bool changingBind = false;
+    public void ChangeBind(string keyName)
     {
-        
+        keyChanging = keyName;
+        StartCoroutine(WaitForBind());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (changingBind)
+        {
+            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    KInputManager.GetKey(keyChanging).AddKey(key);
+                    setBindUI.SetActive(false);
+                    changingBind = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator WaitForBind()
+    {
+        setBindUI.SetActive(true);
+        changingBind = true;
+
+        int seconds = 5;
+        bindTimer.text = "Press a button. (5)";
+        for (int i = seconds; i > 0; i--)
+        {
+            yield return new WaitForSeconds(1);
+            bindTimer.text = "Press a button. (" + i + ")";
+        }
+
+        setBindUI.SetActive(false);
+        changingBind = false;
     }
 }
---
+++ Assets/KeybindButton.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeybindButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI buttonText;
+    [SerializeField] private string keyToChange;
+    
+    
+}

9194e68036cd8f756210d1e723c6e177dbd5f75a
SBSonk <sg.buenaventura04@gmail.com>
2023-05-14 14:54:32 +0800

    Removed an import

--- Assets/ControlsUI.cs
+++ Assets/ControlsUI.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine.Events;
 using UnityEngine.UI;
 

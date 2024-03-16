830458366a8e60e1ac5f1f02909d7ab6ca294180
SBSonk <sg.buenaventura04@gmail.com>
2024-03-17 00:01:31 +0800

    Smart Pistol

---
+++ Assets/HomingProjectile.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingProjectile : Projectile
+{
+    public float homingRange = 3;
+    public float homingStrength = 10;
+    public LayerMask layermask;
+    
+    public Transform lockedEnemy = null;
+    private bool locked = false;
+
+    private void Update()
+    {
+        if (!locked)
+        {
+            // check for area
+            
+            // if found, lock on
+
+            var col = Physics2D.OverlapCircleAll(transform.position, homingRange, layermask);
+
+            foreach (Collider2D c in col)
+            {
+                if (c.TryGetComponent(out Enemy e))
+                {
+                    lockedEnemy = c.transform;
+                    locked = true;
+
+                    break;
+                }
+            }
+        }
+
+        if (lockedEnemy)
+        {
+            // go towards
+            Vector2 dirTowards = (lockedEnemy.position - transform.position).normalized;
+
+            rb.velocity = transform.right * homingStrength;
+            transform.right = Vector3.Lerp(transform.right, dirTowards, Time.deltaTime * homingStrength);
+        }
+    }
+}
